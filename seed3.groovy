787628c8e1c4b2c6cf6199e59bcce5c6c3d1a761
smuvw <srinivasarao_muvva@yahoo.com>
2018-10-11 15:39:30 -0500

    Create seed3.groovy
    test

---
+++ seed3.groovy
@@ -0,0 +1,13 @@
+def utilityLib(){    
+	//loading from the groovy file repository 
+    def util = null
+    util = fileLoader.load('seed2.groovy');
+	}
+   return util
+}
+
+node("master"){
+    def  utilb = utilityLib(
+    utilb.add
+    
+    }
