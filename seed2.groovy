faef56648126dbfc8f8a1132cb8f01043bf01c93
smuvw <srinivasarao_muvva@yahoo.com>
2018-10-12 06:43:29 -0500

    Update seed2.groovy
    excel

--- seed2.groovy
+++ seed2.groovy
@@ -1,8 +1,32 @@
+@Grab('org.apache.poi:poi:3.8')
+@Grab('org.apache.poi:poi-ooxml:3.8')
+@GrabExclude('xml-apis:xml-apis')
+import org.apache.poi.xssf.usermodel.XSSFWorkbook
+import org.apache.poi.xssf.usermodel.XSSFSheet
+
+
 def add() {
  println "welcome Add"
 }
 //return this;
 def sub() {
  println "welcome sub"
+}
+
+
+def readExcel(){
+ def excelFile = new File('Workbook1.xlsx')
+excelFile.withInputStream { is ->
+  workbook = new XSSFWorkbook(is)       
+  (0..<workbook.numberOfSheets).each { sheetNumber ->
+    XSSFSheet sheet = workbook.getSheetAt( sheetNumber )
+    sheet.rowIterator().each { row ->
+      row.cellIterator().each { cell ->
+        println cell.toString()
+      }
+    }
+  }
+}
+
 }
 return this;
