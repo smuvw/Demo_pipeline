0631eb5f9f5e837d2d3a51bb6885c91cd832ab44
smuvw <srinivasarao_muvva@yahoo.com>
2018-10-12 13:34:56 -0500

    Update seed3.groovy
    sdasdas

--- seed3.groovy
+++ seed3.groovy
@@ -1,12 +1,14 @@
 stage 'Load files from GitHub'
 def environment, helloworld
 fileLoader.withGit('https://github.com/smuvw/Demo_pipeline.git', 'master', null, '') {
-    helloworld = fileLoader.load('seed2');
+   // helloworld = fileLoader.load('seed2');
+     helloworld = fileLoader.load('seed5');
    
 }
 
 stage 'Run methods from the loaded content'
 helloworld.add()
 helloworld.sub()
-helloworld.createfile()
+helloworld.Xlsx_Builder(smuvw/Demo_pipeline/output.xlsx)
+helloworld.getRowCount(sheet1)
 
