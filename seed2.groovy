d3ca082d4dc6633984005a79e13b0a500cf7e0c7
smuvw <srinivasarao_muvva@yahoo.com>
2018-10-12 06:56:57 -0500

    Update seed2.groovy
    sasdas

--- seed2.groovy
+++ seed2.groovy
@@ -1,3 +1,27 @@
+import jenkins.model.Jenkins
+import hudson.model.User
+import hudson.security.Permission
+import hudson.EnvVars
+
+
+def createfile() { 
+EnvVars envVars = build.getEnvironment(listener);
+
+filename = envVars.get('WORKSPACE') + "\\node_details.txt";
+//filename = "${manager.build.workspace.remote}" + "\\node_details.txt"
+targetFile = new File(filename);
+println "attempting to create file: $targetFile"
+
+if (targetFile.createNewFile()) {
+    println "Successfully created file $targetFile"
+} else {
+    println "Failed to create file $targetFile"
+}
+print "Deleting ${targetFile.getAbsolutePath()} : "
+println targetFile.delete()
+
+}
+
 def add() {
  println "welcome Add"
 }
