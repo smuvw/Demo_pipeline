a047ccedf54209e3c3d61e76d36d89e3eb26019b
smuvw <srinivasarao_muvva@yahoo.com>
2018-10-12 06:47:38 -0500

    Update seed2.groovy
    asdsa

--- seed2.groovy
+++ seed2.groovy
@@ -1,10 +1,3 @@
-@Grab('org.apache.poi:poi:3.8')
-@Grab('org.apache.poi:poi-ooxml:3.8')
-@GrabExclude('xml-apis:xml-apis')
-import org.apache.poi.xssf.usermodel.XSSFWorkbook
-import org.apache.poi.xssf.usermodel.XSSFSheet
-
-
 def add() {
  println "welcome Add"
 }
