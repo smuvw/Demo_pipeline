102b4bc6bb0e2723b70d32cc6fb93fdf2bf0565e
smuvw <srinivasarao_muvva@yahoo.com>
2018-10-13 20:40:02 -0500

    Update seed6.groovy
    fsdfds

--- seed6.groovy
+++ seed6.groovy
@@ -1,20 +1,4 @@
 parms=null
-pipeline{
-agent any
-stages{
-
-      stage('clean up builds') {
-       steps {
-        dir('notify') {
-        deleteDir()
-        }
-         }
-      }
-
- stage('read') {
- steps {
- script {
- 
   parms=readFile("TestData1.csv").split('\n').each {line ->
   
   def lin =line.split(',')
@@ -37,9 +21,3 @@ stages{
  
  
  }
- }
-}
-}
-
-}
-}
