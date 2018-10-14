8962f54d3b314962e17de0f2791b62ab12c8258c
smuvw <srinivasarao_muvva@yahoo.com>
2018-10-13 20:37:03 -0500

    Create seed6.groovy
    dsfsd

---
+++ seed6.groovy
@@ -0,0 +1,45 @@
+parms=null
+pipeline{
+agent any
+stages{
+
+      stage('clean up builds') {
+       steps {
+        dir('notify') {
+        deleteDir()
+        }
+         }
+      }
+
+ stage('read') {
+ steps {
+ script {
+ 
+  parms=readFile("TestData1.csv").split('\n').each {line ->
+  
+  def lin =line.split(',')
+
+        echo lin[0]
+  echo lin[1]
+    
+  mavenJob(lin[0]) {
+   
+  
+  description(lin[1])
+  properties {
+  githubProjectUrl("http://github.com")
+   
+    
+  }
+ 
+ }
+ 
+ 
+ 
+ }
+ }
+}
+}
+
+}
+}
