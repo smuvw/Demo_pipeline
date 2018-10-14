71bd4a0eb9fe46062addf87a67748bf35587a36d
smuvw <srinivasarao_muvva@yahoo.com>
2018-10-13 21:31:12 -0500

    Update seed2.groovy
    zxczx

--- seed2.groovy
+++ seed2.groovy
@@ -1,17 +1,4 @@
 
-parms=null
-lin[0]=null
-lin[1]=null
-
-def readfileinfo(){
-  parms=readFile("TestData1.csv").split('\n').each {line ->
-  
-  def lin =line.split(',')
-
-        echo lin[0]
-  echo lin[1]
-      
- }
 
 def add() {
  println "welcome Add"
